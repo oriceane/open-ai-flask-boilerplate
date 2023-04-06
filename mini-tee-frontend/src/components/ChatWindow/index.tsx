0e3fe9ad83b8d70c6333b8cb8a09e59b7ce896f5
Ali <awskinner26@gmail.com>
2023-04-06 12:36:30 +0100

    fix thread state issue

--- mini-tee-frontend/src/components/ChatWindow/index.tsx
+++ mini-tee-frontend/src/components/ChatWindow/index.tsx
@@ -1,79 +1,78 @@
-import { Input, Box, Container, Stack, Button, Card } from '@mui/material'
-import React, { SyntheticEvent } from 'react'
-import useSWR from "swr"
+import { Input, Box, Container, Stack, Button, Card } from "@mui/material";
+import React, { SyntheticEvent } from "react";
+import useSWR from "swr";
 
 type Message = {
-    user: UserType;
-    body: string;
-}
+  user: UserType;
+  body: string;
+};
 
-type UserType = 'USER'|'TUTOR'
+type UserType = "USER" | "TUTOR";
 
-const fetchTutorResponse = async (input: string):Promise<string> => {
-    console.log("request - ", input)
-    return await fetch("http://localhost:8888/messages", {
-        method: "POST",
-        body: JSON.stringify(input),
-        headers: {
-            "Content-Type": "application/json",
-          },
-    }).then((res) => res.text());
-}
+const fetchTutorResponse = async (input: string): Promise<string> => {
+  console.log("request - ", input);
+  return await fetch("http://localhost:8888/messages", {
+    method: "POST",
+    body: JSON.stringify(input),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then((res) => res.text());
+};
 
 export const ChatWindow = () => {
-    const [chatThread, setChatThread] = React.useState<Message[]>([])
-    const [isLoading, setIsLoading] =  React.useState<boolean>(false)
+  const [chatThread, setChatThread] = React.useState<Message[]>([]);
+  const [isLoading, setIsLoading] = React.useState<boolean>(false);
 
- 
-    
-    const handleSubmit = async (event: SyntheticEvent<HTMLFormElement>) => {
-        event.preventDefault()
-        
-        const userInput = event.currentTarget.userInput.value
-        const userMessage = {
-            body: userInput,
-            user: "USER" as UserType
-        }
-    
-        const awaitTutorMessage = {
-            body: "...",
-            user: "TUTOR" as UserType
-        }
-        setChatThread([...chatThread, userMessage, awaitTutorMessage])
+  const handleSubmit = (event: SyntheticEvent<HTMLFormElement>) => {
+    event.preventDefault();
 
-        const response = await fetchTutorResponse(userInput)
-        const tutorMessage = {
-            body: response,
-            user: "TUTOR" as UserType
-        }
+    const userInput = event.currentTarget.userInput.value;
+    const userMessage = {
+      body: userInput,
+      user: "USER" as UserType,
+    };
 
-        const thread = chatThread.slice(0, -1)
+    const loadingMessage = {
+      body: "...",
+      user: "TUTOR" as UserType,
+    };
+    const newThreadState = [...chatThread, userMessage, loadingMessage];
+    setChatThread(newThreadState);
 
-        setChatThread([...thread, tutorMessage])
-        console.log(response)
+    fetchTutorResponse(userInput).then((response) => {
+      const tutorMessage = {
+        body: response,
+        user: "TUTOR" as UserType,
+      };
+      console.log(chatThread);
 
-    }
+      setChatThread([...newThreadState.slice(0, -1), tutorMessage]);
+    });
+  };
 
-    return (
+  return (
+    <Box>
+      <Stack>
+        <Box>header</Box>
         <Box>
-            <Stack>
-                <Box>header</Box>
-                <Box>
-                    <Stack component="ul">
-                        {chatThread.map((message) => {
-                            return <Card component="li" key={message.body}>{message.body}</Card>
-                        })}
-                    </Stack>
-                </Box>
-                <Box sx={{border:"1px solid black", padding: "1em"}}>
-                    <form onSubmit={handleSubmit}>
-                    <Input name="userInput" placeholder="Ask for Help"/>
-                    <Button type="submit">submit</Button>
-                    </form>
-                </Box>
-            </Stack>
+          <Stack component="ul">
+            {chatThread.map((message) => {
+              return (
+                <Card component="li" key={message.body}>
+                  {message.body}
+                </Card>
+              );
+            })}
+          </Stack>
         </Box>
-    ) 
-}
-
-
+        <Box sx={{ border: "1px solid black", padding: "1em" }}>
+          <form onSubmit={handleSubmit}>
+            <Input name="userInput" placeholder="Ask for Help" />
+            <Button type="submit">submit</Button>
+          </form>
+        </Box>
+      </Stack>
+    </Box>
+  );
+};
