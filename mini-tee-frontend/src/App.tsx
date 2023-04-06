040741e4d39727d339238ae2f01bb07522512904
Ali <awskinner26@gmail.com>
2023-04-06 10:15:19 +0100

    init react frontend

---
+++ mini-tee-frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Container } from '@mui/material';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+ <Container maxWidth="sm">
+  <h1>MiniTee Chatbot</h1>
+  </Container>
+    </div>
+  );
+}
+
+export default App;
