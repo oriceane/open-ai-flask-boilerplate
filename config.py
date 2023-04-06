f16c011ca588e474b7768eb7bad015014919f32d
Ali <awskinner26@gmail.com>
2023-04-06 10:21:22 +0100

    move key to deperate file and gitignore

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os 
-
+from api_key import key
 UPLOAD_FOLDER = 'static'
 SAVE_FOLDER = 'generated_docs'
 
 ## Enter your Open API Key here
 # OPENAI_API_KEY = os.environ.get('OPENAI_KEY')
-OPENAI_API_KEY = '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'
+OPENAI_API_KEY = key
