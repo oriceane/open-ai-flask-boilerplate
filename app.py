dec1637e1ed4ca269ae2091bc05ab4186c83f253
Oriane Rodriguez <oriane.rodriguez@mytutor.co.uk>
2024-01-19 16:38:25 +0000

    clean up unused stuff

--- app.py
+++ app.py
@@ -1,20 +1,13 @@
 import os
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_cors import CORS, cross_origin
-import aifunctions, googledrivefunctions, musicfunctions, uberduckfunctions, requests
+import aifunctions, uberduckfunctions, requests
 from pathlib import Path
 
 app = Flask(__name__)
 
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
-
-system_prompt = {"role": "system", "content": "You are a tutor, your name is \"Vitu\". You are chatting with your student. Your students are living in England and studying for GCSE exam. You need to help them figure out the answer to the problem by themselves. DON'T GIVE THE ANSWER. Decompose the problems in smaller steps and go through those steps one by one with your student to help them find the answer by themselves.  After each steps ask the students what to do next. Never cover more than one step at the time. Always ask students to try to solve it first without giving any hints or answer. The problem to solve is \"Describe how the lungs are adapted for gas exchange\". Keep your answer brief. Always finish your answer with a follow up question. Encourage your student with a positive tone. Be friendly when you talk. Reassure your students when they don't know the answer or do mistakes. If the student ask for the answer, explain them that \"As a your virtual tutor, it's not my role to give you the answer. However, I can guide you and we can work together to find the answer. Refer yourself as a \"Vitu, your virtual tutor\", never as an \"AI language model\". Never say your an \"AI language model\", you are Vitu, my virtual tutor."}
-assistant_prompt_1 = {"role": "assistant", "content": "Let's try to solve the following problem: \"Describe how the lungs are adapted for gas exchange\"."}
-user_prompt_1 = {"role": "user", "content": "Ok, but please, DON'T GIVE ME THE ANSWER, NEVER!"}
-assistant_prompt_2 = {"role": "assistant", "content": "I can't and won't give you the answer but I can help you. Try to answer the question."}
-assistant_prompt_3 = {"role": "assistant", "content": "What would be your best answer for the following problem \"Describe how the lungs are adapted for gas exchange\"?"}
-all_messages = [system_prompt, assistant_prompt_1, user_prompt_1, assistant_prompt_2, assistant_prompt_3]
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -23,26 +16,6 @@ def page_not_found(e):
 @app.route('/', methods=["GET", "POST"])
 def index():
     return render_template('index.html', **locals())
-
-@app.route('/messages', methods=['POST'])
-@cross_origin()
-def send_message_and_get_response():
-    user_message = request.get_json()
-    all_messages.append({"role": "user", "content": user_message})
-
-    print("MESSAGE: " + user_message)
-
-    options = {}
-    options["messages"] = all_messages
-    response = aifunctions.chatCompletionQuery(options)
-
-    assistant_response = response.choices[0].message.content
-    # assistant_response = "MOCK RESPONSE"
-    all_messages.append({"role": "assistant", "content": assistant_response})
-
-    print("RESPONSE: " + assistant_response)
-
-    return assistant_response
 
 @app.route('/mnemonic', methods=['POST'])
 @cross_origin()
--- uberduckfunctions.py
+++ uberduckfunctions.py
@@ -28,8 +28,6 @@ def get_parsed_lyrics(lyrics):
 def generate_rap(lyrics):
     lyrics_as_list = list(get_parsed_lyrics(lyrics.splitlines()))
 
-#     print(requests.get("https://api.uberduck.ai/voices", params=dict(mode="tts-basic")).json())
-
     backing_tracks = requests.get("https://api.uberduck.ai/reference-audio/backing-tracks", auth=uberduck_auth).json()
     backing_track = random.choice(backing_tracks['backing_tracks'])
     backing_track_uuid = backing_track['uuid']
@@ -39,7 +37,6 @@ def generate_rap(lyrics):
         json=dict(
             lyrics=[lyrics_as_list],
             lines=32,
-#             voicemodel_uuid='4ec5c264-0a49-47c8-a3eb-c0d3518a65a1',
             voicemodel_uuid='3b7973b2-741f-40df-96dc-3cd70c868291',
             backing_track=backing_track_uuid
         ),
